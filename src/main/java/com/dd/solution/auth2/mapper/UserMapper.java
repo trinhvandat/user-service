b8a5a7e165fc38d46be0f1d89e6245eb456fe9af
Leonard <trinhvandat90399@gmail.com>
2021-01-29 18:10:49 +0700

    Update file application.properties

--- src/main/java/com/dd/solution/auth2/mapper/UserMapper.java
+++ src/main/java/com/dd/solution/auth2/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import com.dd.solution.auth2.dto.UserDTO;
 import com.dd.solution.auth2.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 public class UserMapper {
